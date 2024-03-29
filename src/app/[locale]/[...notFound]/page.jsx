9130a1d1375855ddcffed0775389cf8c5af329c9
郑凡 <779965652@qq.com>
2024-03-30 04:06:12 +0800

    not-found

--- src/app/[locale]/[...notFound]/page.jsx
+++ src/app/[locale]/[...notFound]/page.jsx
@@ -2,6 +2,8 @@ import getConfigDataV2 from "@/utils/getConfigDataV2";
 import { cookies } from "next/headers";
 import Main from "./Main";
 
+export const runtime = "edge";
+
 export default async function NotFound({ params: { locale } }) {
   const area = cookies().get("area")?.value || "us";
   const { LANG } = await getConfigDataV2({

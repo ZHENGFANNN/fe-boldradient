ea92dc9ceffbdad3c8c234b0efab946891443ff2
郑凡 <zhengfan@insta360.com>
2024-07-28 02:45:46 +0800

    fix

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -1,22 +1,21 @@
 /** @format */
 import getLanguage from "@/config/LANGUAGE";
-const languageList = getLanguage("list");
+// const languageList = getLanguage("list");
 
-let localeCache = {};
+// let localeCache = {};
 
-async function updateLocaleCache(lang) {
-  const dataModule = await import(`@@/locale/blogData/${lang}.json`);
-  const data = dataModule.default;
-  localeCache[lang] = data;
-}
+// async function updateLocaleCache(lang) {
+//   const dataModule = await import(`@@/locale/blogData/${lang}.json`);
+//   const data = dataModule.default;
+//   localeCache[lang] = data;
+// }
 
-// 初始化缓存
-languageList.forEach((item) => {
-  updateLocaleCache(item.value);
-});
+// // 初始化缓存
+// languageList.forEach((item) => {
+//   updateLocaleCache(item.value);
+// });
 
 export default async function getBlogList(lang) {
-  console.log(`${process.env.NEXT_PUBLIC_DOMAIN}/service/refresh-data`);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_DOMAIN}/service/refresh-data`
   );

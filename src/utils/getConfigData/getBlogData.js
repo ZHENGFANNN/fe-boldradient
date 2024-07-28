5a64805a6ef2688a362728d4e8a24aa0ec70e36f
郑凡 <zhengfan@insta360.com>
2024-07-28 17:46:02 +0800

    fix

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -1,14 +1,21 @@
 /** @format */
 
-export default async function getBlogList(lang) {
+let localeData = null;
+
+async function getData(lang) {
+  if (localeData) return localeData[lang];
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
     {
       method: "GET",
     }
   );
-  console.time("---getBlogList---");
   const data = await response.json();
-  console.timeEnd("---getBlogList---");
+  localeData = data;
   return data[lang];
+}
+
+export default async function getBlogList(lang) {
+  return await getData(lang);
 }

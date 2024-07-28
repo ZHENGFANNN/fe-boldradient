ec3eb730e5429d927480a542852aef2b337f54fc
郑凡 <zhengfan@insta360.com>
2024-07-28 15:59:00 +0800

    featrue

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -2,9 +2,12 @@
 
 export default async function getBlogList(lang) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data?lang=${lang}`,
+    `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
     {
-      method: "GET",
+      method: "POST",
+      body: JSON.stringify({
+        lang,
+      }),
     }
   );
   const data = await response.json();

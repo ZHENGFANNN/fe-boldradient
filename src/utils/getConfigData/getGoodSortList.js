cecfdc8c1142ac8af8c6aa84486b94676f7edfa6
郑凡 <zhengfan@insta360.com>
2024-08-13 01:02:45 +0800

    fix

--- src/utils/getConfigData/getGoodSortList.js
+++ src/utils/getConfigData/getGoodSortList.js
@@ -1,11 +1,29 @@
 /** @format */
 
+const cn = require("@@/locale/productSort/cn.json");
+const de = require("@@/locale/productSort/de.json");
+const en = require("@@/locale/productSort/en.json");
+const es = require("@@/locale/productSort/es.json");
+const fr = require("@@/locale/productSort/fr.json");
+const hk = require("@@/locale/productSort/hk.json");
+const it = require("@@/locale/productSort/it.json");
+const ja = require("@@/locale/productSort/ja.json");
+const ko = require("@@/locale/productSort/ko.json");
+const ru = require("@@/locale/productSort/ru.json");
+
+const productSortList = {
+  cn,
+  de,
+  en,
+  es,
+  fr,
+  hk,
+  it,
+  ja,
+  ko,
+  ru,
+};
+
 export default async function getGoodSortList(lang) {
-  const startTime = Date.now();
-  const { default: productSortList } = await import(
-    "@@/locale/productSort/" + lang + ".json"
-  );
-  const endTime = Date.now();
-  console.log(`getGoodSortList ${lang} time: ${endTime - startTime}`);
-  return productSortList;
+  return productSortList[lang];
 }

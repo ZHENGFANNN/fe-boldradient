543a642ab3bf30b8a597879bb34bac4d92b1e749
郑凡 <zhengfan@insta360.com>
2024-08-13 01:41:17 +0800

    fix

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -1,30 +1,30 @@
 /** @format */
 
-// const localeData = {};
-// async function getData(lang) {
-//   if (!localeData[lang]) {
-//     const response = await fetch(
-//       `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
-//       {
-//         method: "GET",
-//       }
-//     );
-//     const data = await response.json();
-//     localeData[lang] = data[lang];
-//   }
-//   return localeData[lang];
-// }
-
-// export default async function getBlogList(lang) {
-//   const startTime = Date.now();
-//   const data = await getData(lang);
-//   console.log(`---获取Blog时间: ${Date.now() - startTime}---`);
-//   return data;
-// }
+const localeData = {};
+async function getData(lang) {
+  if (!localeData[lang]) {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
+      {
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+    localeData[lang] = data[lang];
+  }
+  return localeData[lang];
+}
 
 export default async function getBlogList(lang) {
   const startTime = Date.now();
-  const data = await import("@@/locale/blogData/" + lang + ".json");
-  console.log(`---获取Blog时间 ${lang}: ${Date.now() - startTime}---`);
+  const data = await getData(lang);
+  console.log(`---获取Blog时间: ${Date.now() - startTime}---`);
   return data;
 }
+
+// export default async function getBlogList(lang) {
+//   const startTime = Date.now();
+//   const data = await import("@@/locale/blogData/" + lang + ".json");
+//   console.log(`---获取Blog时间 ${lang}: ${Date.now() - startTime}---`);
+//   return data;
+// }

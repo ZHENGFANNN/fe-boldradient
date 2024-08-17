ce94e6f0ef57957ad8a224e921d1a3d0ebf3f5a7
郑凡 <zhengfan@insta360.com>
2024-08-17 14:23:02 +0800

    fix

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -19,50 +19,50 @@ async function getDataV3(lang) {
   }
 }
 
-async function getData(lang) {
-  if (!localeData[lang]) {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
-      {
-        method: "GET",
-      }
-    );
-    const data = await response.json();
-    localeData[lang] = data[lang];
-  }
-  return localeData[lang];
-}
+// async function getData(lang) {
+//   if (!localeData[lang]) {
+//     const response = await fetch(
+//       `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
+//       {
+//         method: "GET",
+//       }
+//     );
+//     const data = await response.json();
+//     localeData[lang] = data[lang];
+//   }
+//   return localeData[lang];
+// }
 
-async function getDataV2(lang) {
-  if (!localeData[lang]) {
-    switch (lang) {
-      case "cn":
-        localeData[lang] = await import(`@@/locale/blogData/cn.json`);
-        break;
-      case "en":
-        localeData[lang] = await import(`@@/locale/blogData/en.json`);
-        break;
-      case "ja":
-        localeData[lang] = await import(`@@/locale/blogData/ja.json`);
-        break;
-      case "ko":
-        localeData[lang] = await import(`@@/locale/blogData/ko.json`);
-        break;
-      case "ru":
-        localeData[lang] = await import(`@@/locale/blogData/ru.json`);
-        break;
-      case "fr":
-        localeData[lang] = await import(`@@/locale/blogData/fr.json`);
-        break;
-      case "de":
-        localeData[lang] = await import(`@@/locale/blogData/de.json`);
-        break;
-      default:
-        throw new Error(`Unsupported language: ${lang}`);
-    }
-  }
-  return localeData[lang];
-}
+// async function getDataV2(lang) {
+//   if (!localeData[lang]) {
+//     switch (lang) {
+//       case "cn":
+//         localeData[lang] = await import(`@@/locale/blogData/cn.json`);
+//         break;
+//       case "en":
+//         localeData[lang] = await import(`@@/locale/blogData/en.json`);
+//         break;
+//       case "ja":
+//         localeData[lang] = await import(`@@/locale/blogData/ja.json`);
+//         break;
+//       case "ko":
+//         localeData[lang] = await import(`@@/locale/blogData/ko.json`);
+//         break;
+//       case "ru":
+//         localeData[lang] = await import(`@@/locale/blogData/ru.json`);
+//         break;
+//       case "fr":
+//         localeData[lang] = await import(`@@/locale/blogData/fr.json`);
+//         break;
+//       case "de":
+//         localeData[lang] = await import(`@@/locale/blogData/de.json`);
+//         break;
+//       default:
+//         throw new Error(`Unsupported language: ${lang}`);
+//     }
+//   }
+//   return localeData[lang];
+// }
 
 export default async function getBlogList(lang) {
   const startTime = Date.now();

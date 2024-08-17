ab33dfb4c7b952211403724ce09943fe0013184b
郑凡 <zhengfan@insta360.com>
2024-08-17 14:29:26 +0800

    redis

--- src/utils/getConfigData/getBlogData.js
+++ src/utils/getConfigData/getBlogData.js
@@ -19,19 +19,19 @@ async function getDataV3(lang) {
   }
 }
 
-// async function getData(lang) {
-//   if (!localeData[lang]) {
-//     const response = await fetch(
-//       `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
-//       {
-//         method: "GET",
-//       }
-//     );
-//     const data = await response.json();
-//     localeData[lang] = data[lang];
-//   }
-//   return localeData[lang];
-// }
+async function getData(lang) {
+  if (!localeData[lang]) {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/service/blog/read-blog-data`,
+      {
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+    localeData[lang] = data[lang];
+  }
+  return localeData[lang];
+}
 
 // async function getDataV2(lang) {
 //   if (!localeData[lang]) {
@@ -66,7 +66,7 @@ async function getDataV3(lang) {
 
 export default async function getBlogList(lang) {
   const startTime = Date.now();
-  const data = await getDataV3(lang);
+  const data = await getData(lang);
   console.log(`---获取Blog时间: ${Date.now() - startTime}---`);
   return data;
 }
